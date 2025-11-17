518d18c509ce6f95646f58f1c55ce4766171bea9
hirokamuy <iamhrat@gmail.com>
2025-11-17 15:54:59 +0900

    revise a.cpp

--- a.cpp
+++ a.cpp
@@ -1,10 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-using pint = pair<int,int>;
-using ll = long long int;
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vd vector<double>
+#define vvd vector<vector<double>>
+#define pii pair<int,int>
+#define int long long
 #define rep(i,n) for(int i=0;i<n;i++)
 
-int main(void)
-{
+void solve(void){
+
+}
+
+signed main(void){
+	solve();
 	return 0;
 }
