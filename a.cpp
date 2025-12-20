dd2586bace5f7058f901ef501ea78740d83867e4
hirokamuy <iamhrat@gmail.com>
2025-12-20 17:32:39 +0900

    add macro

--- a.cpp
+++ a.cpp
@@ -1,14 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define int long long
+
 #define vi vector<int>
 #define vvi vector<vector<int>>
 #define vd vector<double>
 #define vvd vector<vector<double>>
 #define pii pair<int,int>
+#define pid pair<int,double>
+#define pdd pair<double,double>
 #define vpii vector<pair<int,int>>
-#define int long long
+#define vpid vector<pair<int,double>>
+
 #define rep(i,n) for(int i=0;i<n;i++)
-#define FOR(i,k,n) for(int i=k;i<n;i++)
+#define repi(i,k,n) for(int i=k;i<n;i++)
 #define NO cout<<"No"<<endl
 #define YES cout<<"Yes"<<endl
 #define ALL(v) v.begin(),v.end()
