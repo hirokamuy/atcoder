a4b1b59ad7043fd8d846eb0555ac922eab1400c4
hirokamuy <iamhrat@gmail.com>
2025-12-22 16:24:42 +0900

    revise macro

--- a.cpp
+++ a.cpp
@@ -6,11 +6,9 @@ using namespace std;
 #define vvi vector<vector<int>>
 #define vd vector<double>
 #define vvd vector<vector<double>>
+#define vs vector<string>
 #define pii pair<int,int>
-#define pid pair<int,double>
-#define pdd pair<double,double>
 #define vpii vector<pair<int,int>>
-#define vpid vector<pair<int,double>>
 
 #define rep(i,n) for(int i=0;i<n;i++)
 #define repi(i,k,n) for(int i=k;i<n;i++)
